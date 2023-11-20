9d84d84ea96d82664fe497b2b32155100505fa07
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-20 16:36:19 +0300

    routing request
    32

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first response page</title></head>");
+    res.write(
+      "<body><h1>Main Page</h1><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   //process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
