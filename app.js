8a94a1137a1177fb35a3a5dc8ac166722a6c01c4
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-20 16:44:12 +0300

    redireting request
    33

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -10,6 +12,12 @@ const server = http.createServer((req, res) => {
       "<body><h1>Main Page</h1><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
     );
     res.write("</html>");
+    return res.end(); // to not move other lines
+  }
+  if (url === "/message" && method == "POST") {
+    fs.writeFileSync("message.tsx", "dummy");
+    res.statusCode = 302;
+    res.setHeader = ("Location", "/");
     return res.end();
   }
   //process.exit();
