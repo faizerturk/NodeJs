4821d9cd2eda87dd8a62ba3330157b186a5e82a9
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-21 18:28:13 +0300

    Node Module System
    38

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My first response page</title></head>");
-    res.write(
-      "<body><h1>Main Page</h1><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end(); // to not move other lines
-  }
-  if (url === "/message" && method == "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      //writeFileSync, the synchronous mode and we block execution of the next line of code until this file is done
-      //fs.writeFileSync("message.tsx", message);
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader = ("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  //process.exit();
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first response page</title></head>");
-  res.write("<body><h1>Response</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
