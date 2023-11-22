3a77baffd7af8f5270d483de3150448bbea66139
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-22 20:53:04 +0300

    handling routes Express.js
    63

--- Express.js/app.js
+++ Express.js/app.js
@@ -2,19 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //allows the request to continue to the next  middleware in line
+app.use("/add-product", (req, res, next) => {
+  console.log("heree!");
+  res.send("<h1>Product!</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("heree!");
   res.send("<h1>Hello from Express!</h1>");
 });
-
-// const server = http.createServer(app);
-// server.listen(3000);
-
-// the two lines of vanilla node.js code above are equal to the express.js code below
 
 app.listen(3000);
