aa2dfe41d11e349b0393c22bf24180c53df5cfa2
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-22 19:59:11 +0300

    adding middleware
    60

--- Express.js/app.js
+++ Express.js/app.js
@@ -1,3 +1,17 @@
-const server = http.createServer();
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); //allows the request to continue to the next  middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("heree!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
