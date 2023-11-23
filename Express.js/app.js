68379c1f9237c58cca195ef7549e03aa2e044886
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-23 16:20:59 +0300

    filtering paths
    68

--- Express.js/app.js
+++ Express.js/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found!</h1>");
