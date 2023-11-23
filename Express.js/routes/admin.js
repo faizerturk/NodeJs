3e6927b16ef18e59a82e2fb5821f0c43489aa928
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-23 20:23:04 +0300

    using helper function for navigation
    73

--- Express.js/routes/admin.js
+++ Express.js/routes/admin.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-
+const rootDir = require("../util/path");
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
