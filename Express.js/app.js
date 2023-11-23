e5a8469cb8dddb9788984d33d6fcce8d17c59b1a
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-23 22:06:06 +0300

    notes

--- Express.js/app.js
+++ Express.js/app.js
@@ -7,7 +7,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //tells the Express.js app to serve static files from the "public" directory located in the same directory as the executing script. This is useful for when you want to include files like images, CSS, and JavaScript in your web application without having to define routes for each of them. When a file is requested, Express looks for it in the "public" directory first. If it finds the file, it sends it to the client; if not, it moves on to the next middleware.
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
--- Express.js/util/path.js
+++ Express.js/util/path.js
@@ -1,3 +1,6 @@
 const path = require("path");
 
 module.exports = path.dirname(require.main.filename);
+
+// It is a structure used in Node.js applications and is used to find the root directory path of the application.
+//its return /Users/MyUserName/Projects/GitHub/NodeJs/Express
