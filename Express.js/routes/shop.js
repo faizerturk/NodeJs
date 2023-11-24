b29fd295a57251af30b4126a142baceaef546a7b
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-24 17:01:14 +0300

    PUG : templating engine
    81-86

--- Express.js/routes/shop.js
+++ Express.js/routes/shop.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const path = require("path");
-const rootDir = require("../util/path");
 const router = express.Router();
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log("adminData.producst", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+  });
 });
 
 module.exports = router;
