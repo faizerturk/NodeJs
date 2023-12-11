6d973ca648b8006c67a88bb142b6d764f66ec2e2
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-11 23:08:16 +0300

    displaying product data
    111

--- MVC/controllers/admin.js
+++ MVC/controllers/admin.js
@@ -21,6 +21,7 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
+  console.log(req.body);
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
