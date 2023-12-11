7ab463c23c0c44a760a3e5f670dd3e55e9cc0c48
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-11 23:13:17 +0300

    fix

--- MVC/controllers/admin.js
+++ MVC/controllers/admin.js
@@ -21,12 +21,11 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body);
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
