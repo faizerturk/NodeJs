7830155dd6f09b8a3d6fdc2d79bea2edddfdde98
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-14 16:39:29 +0300

    adding the product-delete functionality

--- MVC/modals/product.js
+++ MVC/modals/product.js
@@ -47,6 +47,16 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
