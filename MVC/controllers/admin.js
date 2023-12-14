0257c84583774414586880faf8f9929d1505e839
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-14 16:28:10 +0300

    editing the product data
    127

--- MVC/controllers/admin.js
+++ MVC/controllers/admin.js
@@ -4,34 +4,29 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
     editing: false,
   });
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect("/");
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-
   if (!editMode) {
     return res.redirect("/");
   }
-
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
     if (!product) {
-      res.redirect("/");
+      return res.redirect("/");
     }
     res.render("admin/edit-product", {
       pageTitle: "Edit Product",
@@ -42,14 +37,29 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-exports.postEditProduct = (req, res, next) => {};
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
 
-exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
 };
--- MVC/routes/admin.js
+++ MVC/routes/admin.js
@@ -1,6 +1,10 @@
+const path = require("path");
+
 const express = require("express");
-const router = express.Router();
+
 const adminController = require("../controllers/admin");
+
+const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", adminController.getAddProduct);
@@ -11,9 +15,8 @@ router.get("/products", adminController.getProducts);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
-// /admin/edit-product => GET
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.post("edit-product", adminController.postEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 
 module.exports = router;
