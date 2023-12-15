8340fbc14c094ec4287d65b6e436983eaf3f67f5
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-15 12:13:54 +0300

    Deleting cart items
    129

--- MVC/modals/cart.js
+++ MVC/modals/cart.js
@@ -15,14 +15,12 @@ module.exports = class Cart {
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
         (prod) => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-
       // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
@@ -37,6 +35,37 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find((prod) => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
