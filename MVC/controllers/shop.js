488159769c3bd4c2a96b2fd675c53c9af5b4a376
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-15 12:43:35 +0300

    displaying cart items on the cart page
    132-133

--- MVC/controllers/shop.js
+++ MVC/controllers/shop.js
@@ -33,9 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Card",
-    path: "/cart",
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
+    });
   });
 };
 
@@ -47,16 +61,24 @@ exports.postCart = (req, res, next) => {
   res.redirect("/cart");
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
-    pageTitle: "Your Orders",
     path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
-    pageTitle: "Checkout",
     path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
