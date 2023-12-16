baa54c97254367185f23ef2fe062f436177fb01f
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-16 14:39:46 +0300

    fetching a single product with the 'where' condition
    146

--- MVC-SQL/controllers/shop.js
+++ MVC-SQL/controllers/shop.js
@@ -10,18 +10,20 @@ exports.getProducts = (req, res, next) => {
         path: "/products",
       });
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
-    });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
