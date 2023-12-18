fd409af86b7071866572120195c351ee2b363388
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-18 17:35:18 +0300

    Fetching admin products
    158

--- MVC-SQL/controllers/admin.js
+++ MVC-SQL/controllers/admin.js
@@ -62,15 +62,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("admin/products", {
-        prods: rows,
+        prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
       });
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
