9479c380468a35e39210253320f87362891ff9e5
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-18 16:15:47 +0300

    sequelize - connecting to the database
    150-151

--- MVC-SQL/util/database.js
+++ MVC-SQL/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "nodeComplete", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "nodeComplete",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
