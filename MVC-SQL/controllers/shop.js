e1b7212d07b4156c1ac96548d46f9c91c10237c0
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-18 17:10:15 +0300

    retrieving data and finding products
    156

--- MVC-SQL/controllers/shop.js
+++ MVC-SQL/controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require("../modals/product");
 const Cart = require("../modals/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/product-list", {
-        prods: rows,
+        prods: products,
         pageTitle: "All Products",
         path: "/products",
       });
@@ -27,15 +27,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
