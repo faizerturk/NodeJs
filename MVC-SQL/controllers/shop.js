eb49d38c191ea7621700bc644584e3169373b771
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-21 14:04:18 +0300

    completing the MVC-SQL mysql part

--- MVC-SQL/controllers/shop.js
+++ MVC-SQL/controllers/shop.js
@@ -128,9 +128,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -149,6 +151,9 @@ exports.postOrder = (req, res, next) => {
         });
     })
     .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect("orders");
     })
     .catch((err) => {
@@ -157,15 +162,16 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
