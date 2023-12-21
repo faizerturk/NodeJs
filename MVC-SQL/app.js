7c36d30a1f2ae53eea05e7ca7439738843b5bcdd
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-21 09:12:05 +0300

    creating & fetching a cart
    167

--- MVC-SQL/app.js
+++ MVC-SQL/app.js
@@ -42,8 +42,8 @@ Cart.belongsToMany(Product, { through: CartItem }); // {through: CartItem} -> te
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  //.sync()
+  //.sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
     //console.log(result);
@@ -57,6 +57,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
