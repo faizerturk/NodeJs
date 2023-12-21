eb174175ef2d80c189b044cf3675ade32a7b39f2
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-21 13:13:25 +0300

    Storing cartItems as orderItems
    172

--- MVC-SQL/controllers/shop.js
+++ MVC-SQL/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../modals/product");
-const Cart = require("../modals/cart");
+const Order = require("../modals/order");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -121,6 +121,35 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .then((result) => {
       res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .then((result) => {
+      res.redirect("orders");
     })
     .catch((err) => {
       console.log(err);
