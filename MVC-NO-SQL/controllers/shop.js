9bc80af46a4905285c77491e12c343608717644e
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-01-15 14:18:16 +0300

    bug fix

--- MVC-NO-SQL/controllers/shop.js
+++ MVC-NO-SQL/controllers/shop.js
@@ -74,6 +74,7 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
+      res.redirect("/cart");
     })
     .catch((err) => {
       console.log(err);
