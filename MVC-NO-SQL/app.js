b185ef6e5dec3856db076e609fd67b82809e380d
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-01-18 23:31:51 +0300

    saving data through mongoose
    213

--- MVC-NO-SQL/app.js
+++ MVC-NO-SQL/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const User = require('./models/user');
+// const User = require('./models/user');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -14,17 +14,17 @@ const errorController = require('./controllers/error');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('65a119b7cc42b79ee1540e22')
-    .then((user) => {
-      console.log('User found', user);
-      req.user = new User(user.username, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById('65a119b7cc42b79ee1540e22')
+//     .then((user) => {
+//       console.log('User found', user);
+//       req.user = new User(user.username, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
