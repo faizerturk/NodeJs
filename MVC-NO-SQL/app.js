9a36c65ee63a93a12feb281c92f0ef6f90289c1a
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-01-18 22:30:27 +0300

    connecting to the MongoDB Server with Mongoose
    211

--- MVC-NO-SQL/app.js
+++ MVC-NO-SQL/app.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./modals/user");
-app.set("view engine", "ejs");
-app.set("views", "views");
+const mongoose = require('mongoose');
+const User = require('./modals/user');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("65a119b7cc42b79ee1540e22")
+  User.findById('65a119b7cc42b79ee1540e22')
     .then((user) => {
-      console.log("User found", user);
+      console.log('User found', user);
       req.user = new User(user.username, user.email, user.cart, user._id);
       next();
     })
@@ -26,12 +26,18 @@ app.use((req, res, next) => {
     });
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  //console.log("client", client);
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://faize:r3RGN8OFfU2IiEl5@cluster0.zhfufyo.mongodb.net/shop?retryWrites=true'
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
