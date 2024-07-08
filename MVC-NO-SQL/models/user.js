32cf92d82923bbd87dc9d7b921620b8fdf0c18dd
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-08 15:16:53 +0300

    213
    creating the product schema

--- MVC-NO-SQL/models/user.js
+++ MVC-NO-SQL/models/user.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+});
+
+//--------without mongoose------
+
 // const mongodb = require("mongodb");
 // const getDb = require("../util/database").getDb;
 
