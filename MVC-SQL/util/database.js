1d48520d6db947d3b83cd42571e2c2e55328784e
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-08 16:46:12 +0300

    MVC-SQL has been removed

--- MVC-SQL/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("node-complete", "root", "nodeComplete", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
