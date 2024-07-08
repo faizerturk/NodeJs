027e955b642ad80f35b587af13bce41b634a4542
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-08 16:25:12 +0300

    deleting products
    218

--- MVC-NO-SQL/controllers/admin.js
+++ MVC-NO-SQL/controllers/admin.js
@@ -87,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log('Destroyed product');
       res.redirect('/admin/products');
