224103d014db8d70e308d2579bec53ce1186a189
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-08 16:41:21 +0300

    express.js golder has been removed

--- Express.js/util/path.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-module.exports = path.dirname(require.main.filename);
-
-// It is a structure used in Node.js applications and is used to find the root directory path of the application.
-//its return /Users/MyUserName/Projects/GitHub/NodeJs/Express
