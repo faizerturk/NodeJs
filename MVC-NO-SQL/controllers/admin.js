8583335af0e7d38b3089f508d3ecd0ce98766e33
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-08 16:19:19 +0300

    updating products
    217

--- MVC-NO-SQL/controllers/admin.js
+++ MVC-NO-SQL/controllers/admin.js
@@ -58,16 +58,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedImageUrl,
-    updatedPrice,
-    updatedDesc,
-    prodId
-  );
-
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.description = updatedDesc;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
     .then((result) => {
       console.log('Updated product');
       res.redirect('/admin/products');
@@ -76,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render('admin/products', {
         prods: products,
