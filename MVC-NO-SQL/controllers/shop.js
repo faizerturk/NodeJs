2feab406bc21ff1c0d52d9fc5725887094446565
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-11 23:52:32 +0300

    Clearing the cart after storing an order

--- MVC-NO-SQL/controllers/shop.js
+++ MVC-NO-SQL/controllers/shop.js
@@ -145,6 +145,9 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then((result) => {
+      req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch((err) => console.log(err));
