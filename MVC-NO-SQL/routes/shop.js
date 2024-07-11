5c4ff159d0b30bef3d71b6a96b3b9b8e370034b8
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-07-11 23:10:15 +0300

    loading the cart

--- MVC-NO-SQL/routes/shop.js
+++ MVC-NO-SQL/routes/shop.js
@@ -5,7 +5,7 @@ const shopController = require('../controllers/shop');
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
-// router.get("/cart", shopController.getCart);
+router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 // router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 // router.post("/create-order", shopController.postOrder);
